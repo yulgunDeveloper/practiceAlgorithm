9580b2053ba7331f193fab9151378c24a0f68301
yulkun <green4652@naver.com>
2022-12-28 16:15:38 +0900

    intellij에서 실행

---
+++ practiceAlgorithm/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("여기다가 해야 java 실행 됌");
+    }
+}
---
+++ practiceAlgorithm/src/bakjun/bak5597/Main.java
@@ -0,0 +1,7 @@
+package bakjun.bak5597;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("여기다가 해야 java 실행 됌");
+    }
+}
