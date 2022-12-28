314c0884eefe744a2dfa8c5ddee0117821c7e235
yulkun <green4652@naver.com>
2022-12-28 17:09:11 +0900

    intellij에서 실행

--- practiceAlgorithm/src/bakjun/bak5597/Main.java
+++ practiceAlgorithm/src/bakjun/bak5597/Main.java
@@ -1,7 +1,30 @@
 package bakjun.bak5597;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("여기다가 해야 java 실행 됌");
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 28; i++) {
+            list.add(sc.nextInt());
+        }
+        Collections.sort(list);
+        int j = 1;
+        int k = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) != j) {
+                System.out.println(j);
+                j++;
+                k++;
+            }
+            j++;
+        }
+        if (k < 2) {
+            System.out.println(30);
+        }
     }
 }
