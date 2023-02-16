730e7f12c2b23c8a266816051d09ac6b98105784
yulkun <green4652@naver.com>
2023-02-16 11:09:01 +0900

    leetcode 69

--- practiceAlgorithm/src/leetcode/Easy69/Main.java
+++ practiceAlgorithm/src/leetcode/Easy69/Main.java
@@ -2,5 +2,18 @@ package leetcode.Easy69;
 
 public class Main {
     public static void main(String[] args) {
+        int x = 2147395600;
+        int max = 1;
+        long a = 0L;
+        long b = 0L;
+        for (int i = 0; i <= x; i++) {
+            a = Long.valueOf(i) * Long.valueOf(i);
+            b = Long.valueOf(max) * Long.valueOf(max);
+            if (a <= x && b > x) {
+                System.out.println(i);
+            }
+            max++;
+        }
+        System.out.println(max);
     }
 }
