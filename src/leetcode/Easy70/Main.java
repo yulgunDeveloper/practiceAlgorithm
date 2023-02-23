c9cd3008b7023194fd88037583cb782e8035b598
yrkwoun <green4652@naver.com>
2023-02-23 10:45:52 +0900

    70
    ... 봐도 이해가 안감..

--- src/leetcode/Easy70/Main.java
+++ src/leetcode/Easy70/Main.java
@@ -2,7 +2,16 @@ package leetcode.Easy70;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("70");
+        int n = 4;
+        if (n <= 1) {
+            System.out.println(1);
+        }
+        int[] list = new int[n+1];
+        list[0] = 1;
+        list[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            list[i] = list[i-1] + list[i-2];
+        }
+        System.out.println(list[n]);
     }
 }
