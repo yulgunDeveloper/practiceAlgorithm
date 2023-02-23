e3b14477304abf855b051e012becefe277ccec15
yrkwoun <green4652@naver.com>
2023-02-23 10:36:54 +0900

    70

--- src/leetcode/Easy70/Main.java
+++ src/leetcode/Easy70/Main.java
@@ -1,10 +1,8 @@
 package leetcode.Easy70;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("70");
     }
 }
