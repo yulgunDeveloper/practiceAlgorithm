f6606930255793e548b61d5d5fa97add67e7703b
yrkwoun <green4652@naver.com>
2023-02-28 11:14:11 +0900

    leetcode 168...

--- src/leetcode/Easy168/Main.java
+++ src/leetcode/Easy168/Main.java
@@ -1,34 +1,17 @@
 package leetcode.Easy168;
 
-
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        int columnNumber = 703; // AAA
+        int columnNumber = 701; // ZY
 
-        List<Integer> list = new ArrayList<>();
-        int quotient = 0;
-        int remainder = 0;
-        char c = 0;
         String result = "";
-        quotient = columnNumber / 26;
-        remainder = columnNumber % 26;
-        System.out.println(quotient);
-        System.out.println(remainder);
-        if (quotient == 0) {
-            c = (char) (columnNumber + 64);
-            result = String.valueOf(c);
-            System.out.println(result);
-        } else {
-//            c = (char) (quotient + 64);
-//            result = String.valueOf(c);
-//            c = (char) (remainder + 64);
-//            result += String.valueOf(c);
-//            System.out.println(result);
 
-            System.out.println(list.get(0));
-            System.out.println(quotient);
+        while (columnNumber > 0) {
+            int remainder = columnNumber % 26;
+            char character = (char) (remainder + 64);
+            result = character + result;
+            columnNumber = columnNumber / 26;
         }
+        System.out.println(result);
     }
 }
