4c0d0bcaac0f3aedd8a0658661246573f3491e14
yrkwoun <green4652@naver.com>
2023-03-10 15:50:17 +0900

    programmers

---
+++ src/programmers/pro155652/Main.java
@@ -0,0 +1,11 @@
+package programmers.pro155652;
+
+public class Main {
+    public static void main(String[] args) {
+        String s ="aukks";
+        String skip = "wbqd";
+        int index = 5;
+
+
+    }
+}
