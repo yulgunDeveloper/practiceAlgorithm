21eb9dee34637a64123cc0606bb11c39a9388ae5
yrkwoun <green4652@naver.com>
2023-03-16 10:13:44 +0900

    programmers 155652

--- src/programmers/pro155652/Main.java
+++ src/programmers/pro155652/Main.java
@@ -1,10 +1,41 @@
 package programmers.pro155652;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         String s ="aukks";
         String skip = "wbqd";
         int index = 5;
 
+        List<Character> skipList = new ArrayList<>();
+        List<Character> result;
+        String answer = "";
+
+
+        for (int i = 0; i < skip.length(); i++) {
+            char ch = skip.charAt(i);
+            skipList.add(ch);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            result = new ArrayList<>();
+            char one = s.charAt(i);
+            for (int j = 0; j < index; j++) {
+                one++;
+                if (one >= 123) one -= 26;
+                if (!skipList.contains(one)) {
+                    result.add(one);
+                }
+            }
+            char last = result.get(result.size() - 1);
+            if (result.size() < index) {
+                int cnt = index - result.size();
+                last += cnt;
+                if (last >= 123) last -= 26;
+            }
+            answer += String.valueOf(last);
+        }
+        System.out.println(answer);
     }
 }
