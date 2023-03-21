d7f3931d17d07ca5d96947478d5d823d529522d2
yrkwoun <green4652@naver.com>
2023-03-21 14:45:47 +0900

    programmers 155652

--- src/programmers/pro155652/Main.java
+++ src/programmers/pro155652/Main.java
@@ -4,44 +4,44 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String s ="eeefgaukks";
+        String s ="aukks";
         String skip = "wbqd";
         int index = 5;
 
-        List<Character> skipList = new ArrayList<>();
-        List<Character> result;
         String answer = "";
-
+        List<Character> skipList = new ArrayList<>();
+        int isSkip = 0;
+        boolean tf = true;
 
         for (int i = 0; i < skip.length(); i++) {
-            char ch = skip.charAt(i);
-            if (!skipList.contains(ch)) {
-                skipList.add(ch);
-            }
+            char skipCh = skip.charAt(i);
+            skipList.add(skipCh);
         }
 
         for (int i = 0; i < s.length(); i++) {
-            result = new ArrayList<>();
-            char one = s.charAt(i);
+            isSkip = 0;
+            char compareCh = s.charAt(i);
             for (int j = 0; j < index; j++) {
-                one = isNotSkip(one);
-                if (!skipList.contains(one)) {
-                    result.add(one);
+                compareCh = isNotSkip(compareCh);
+                if (skipList.contains(compareCh)){
+                    isSkip++;
                 }
             }
-            char last = result.get(result.size() - 1);
-            int cnt = index - result.size();
-            if (cnt > 0) {
-                last += cnt;
-                if (last >= 123) last -= 26;
+            compareCh += isSkip;
+            if (compareCh >= 123) compareCh -= 26;
+            if (skipList.contains(compareCh)) {
+                while (tf) {
+                    compareCh = isNotSkip(compareCh);
+                    if (!skipList.contains(compareCh)) {
+                        tf = false;
+                    }
+                }
             }
-            while (skipList.contains(last)) {
-                last = isNotSkip(last);
-            }
-            answer += String.valueOf(last);
+            answer += String.valueOf(compareCh);
         }
         System.out.println(answer);
     }
+
     public static char isNotSkip(char ch) {
         ch++;
         if (ch >= 123) ch -= 26;
